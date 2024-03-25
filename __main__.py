773702c81cd579e2439796dbdde25f71e413770c
teethut <teethutbie@gmail.com>
2024-03-25 14:50:50 +0700

    update

--- __main__.py
+++ __main__.py
@@ -18,20 +18,21 @@ running=True
 display_text="Test"
 text_style=pygame.font.SysFont("Arial",30,bold=True)
 text_color=(255,255,255)
-text_position=(100,200)
+text_position=(100,100)
 def create_txt(text,font,color,coor):
     text_img=font.render(text,False,color)
     screen.blit(text_img,(coor[0],coor[1]-50))
 
 #platform_length=int(input("enter platform length:"))
 
+platform_max_height=300
+platform_height=platform_max_height*(sum(selected_people)/platform_limit)
+platform_length=500
+recPlatform=pygame.Rect(250,600-platform_height,platform_length,platform_height)
+
 
 person_height=80
 person_width=30
-
-
-platform_length=500
-rec1=pygame.Rect(250,400,platform_length,50)
 recP=pygame.Rect(0,0,person_width,person_height)
 
 while running:
@@ -43,38 +44,39 @@ while running:
         else:
             running=True
     
-    create_txt("Platform weight limit: "+str(platform_limit),text_style,text_color,text_position)
-    
-    
+    create_txt("Platform Weight Limit: "+str(platform_limit),text_style,text_color,text_position)
+    create_txt("Person's Weight: "+str(selected_people),text_style,text_color,(100,200))
+    create_txt("Current Weight: "+str(sum(selected_people)),text_style,(255,255-255*(sum(selected_people)/platform_limit),255-255*(sum(selected_people)/platform_limit)),(100,250)) #red if max load, whight if no load
+
     pos= pygame.mouse.get_pos()
-    if rec1.collidepoint(pos)&pygame.mouse.get_pressed()[0]: #hold the mouse left-click to expand the platform
+    if recPlatform.collidepoint(pos)&pygame.mouse.get_pressed()[0]: #hold the mouse left-click to expand the platform
         col=(0,255,0)
         #platform_length=300
-        rec1.inflate_ip(5, 0)
-        #rec1.update(250,400,platform_length,50)
+        recPlatform.inflate_ip(5, 0)
+        #recPlatform.update(250,400,platform_length,50)
         #platform_length=300
-        #rec1=pygame.Rect(250,400,350,50)
+        #recPlatform=pygame.Rect(250,400,350,50)
         
-        #pygame.draw.rect(screen,col,rec1)
-    elif rec1.collidepoint(pos)&pygame.mouse.get_pressed()[2]: #hold the mouse right-click to shrink the platform
+        #pygame.draw.rect(screen,col,recPlatform)
+    elif recPlatform.collidepoint(pos)&pygame.mouse.get_pressed()[2]: #hold the mouse right-click to shrink the platform
         col=(255,0,0)
-        rec1.inflate_ip(-5, 0)    
+        recPlatform.inflate_ip(-5, 0)    
     else:
         col=(0,0,255)
         #platform_length=400
-        #pygame.draw.rect(screen,col,rec1)
+        #pygame.draw.rect(screen,col,recPlatform)
 
-    pygame.draw.rect(screen,col,rec1)
+    pygame.draw.rect(screen,col,recPlatform)
 
     for i in range(0,len(selected_people)):   #placing people on the platform
         if len(selected_people)==1:
-            recP.center=(250+person_width/2+platform_length/2,320+40)
+            recP.center=(250+person_width/2+platform_length/2,600-platform_height-40)
             pygame.draw.rect(screen,(255,255,0),recP,width=0)
         if len(selected_people)==2:
-            recP.center=(250+person_width/2+i*(platform_length-person_width),320+40)
+            recP.center=(250+person_width/2+i*(platform_length-person_width),600-platform_height-40)
             pygame.draw.rect(screen,(255,255,0),recP,width=0)
         if len(selected_people)==3:
-            recP.center=(250+person_width/2+i*(platform_length-person_width)/2,320+40)
+            recP.center=(250+person_width/2+i*(platform_length-person_width)/2,600-platform_height-40)
             pygame.draw.rect(screen,(255,255,0),recP,width=0)   
         
     pygame.display.update()
