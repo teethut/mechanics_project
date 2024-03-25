b16837fe5c6b39efc1069ea370913e0b2348395e
teethut <teethutbie@gmail.com>
2024-03-25 12:09:52 +0700

    update t

--- __main__.py
+++ __main__.py
@@ -23,10 +23,16 @@ def create_txt(text,font,color,coor):
     text_img=font.render(text,False,color)
     screen.blit(text_img,(coor[0],coor[1]-50))
 
-platform_length=int(input("enter platform length:"))
+#platform_length=int(input("enter platform length:"))
+
 
 person_height=80
 person_width=30
+
+
+platform_length=500
+rec1=pygame.Rect(250,400,platform_length,50)
+recP=pygame.Rect(0,0,person_width,person_height)
 
 while running:
     for event in pygame.event.get():
@@ -34,19 +40,35 @@ while running:
             running=False
         else:
             running=True
-
-    create_txt(display_text,text_style,text_color,text_position)
-    rec1=pygame.draw.rect(screen,(255,0,0),(250,400,platform_length,50),width=0)
+    
+    create_txt("Platform weight limit: "+str(platform_limit),text_style,text_color,text_position)
 
     for i in range(0,len(selected_people)):   #placing people on the platform
         if len(selected_people)==1:
-            pygame.draw.rect(screen,(255,255,0),(250+platform_length/2,320,person_width,person_height),width=0)
+            recP.center=(250+person_width/2+platform_length/2,320+40)
+            pygame.draw.rect(screen,(255,255,0),recP,width=0)
         if len(selected_people)==2:
-            pygame.draw.rect(screen,(255,255,0),(250+i*(platform_length-person_width),320,person_width,person_height),width=0)
+            recP.center=(250+person_width/2+i*(platform_length-person_width),320+40)
+            pygame.draw.rect(screen,(255,255,0),recP,width=0)
         if len(selected_people)==3:
-            pygame.draw.rect(screen,(255,255,0),(250+i*(platform_length-person_width)/2,320,person_width,person_height),width=0)
+            recP.center=(250+person_width/2+i*(platform_length-person_width)/2,320+40)
+            pygame.draw.rect(screen,(255,255,0),recP,width=0)
+    
+    
+    pos= pygame.mouse.get_pos()
+    if rec1.collidepoint(pos)&pygame.mouse.get_pressed()[0]:
+        col=(100,255,50)
+        platform_length=300
+        #rec1=pygame.Rect(250,400,350,50)
+        
+        pygame.draw.rect(screen,col,rec1)
 
+    else:
+        col=(255,0,0)
+        #platform_length=400
+        pygame.draw.rect(screen,col,rec1)
 
+        
     pygame.display.update()
     pygame.time.delay(100)
 
